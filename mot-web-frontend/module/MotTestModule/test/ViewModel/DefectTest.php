d3f074ffeff62f0db51f168853e27b2e52be1b6a
DVSA <dvsa@dvsa.gov.uk>
2017-11-20 17:11:46 +0000

    BL-6519 and BL-6538 - Assigning specific RFR IDs to the new EU Directive Special Processing RFRs in advance of Phase C Sprint 5.

--- mot-web-frontend/module/MotTestModule/test/ViewModel/DefectTest.php
+++ mot-web-frontend/module/MotTestModule/test/ViewModel/DefectTest.php
@@ -8,37 +8,97 @@ use DvsaEntities\Entity\RfrDeficiencyCategory;
 
 class DefectTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCreation()
+    const DEFECT_ID = 10;
+    const PARENT_CATEGORY_ID = 20;
+    const DESCRIPTION = 'Description';
+    const ADVISORY_TEXT = 'Advisory text';
+    const DEFAULT_BREADCRUMBS = 'asd';
+    const INSPECTION_MANUAL_REFERENCE = 'Inspection manual reference';
+    const INSPECTION_MANUAL_REFERENCE_URL = 'http://noot.com';
+    const NEW_BREADCRUMBS = 'breadcrumb';
+
+    /**
+     * @dataProvider testCreationDP
+     */
+    public function testCreation($isAdvisory, $isPrs, $isFailure, $deficiencyCategory)
     {
         $defect = new Defect(
-            10,
-            20,
-            'Description',
-            'asd',
-            'Advisory text',
-            'Inspection manual reference',
-            true,
-            false,
-            true,
-            RfrDeficiencyCategoryCode::PRE_EU_DIRECTIVE,
-            true
+            self::DEFECT_ID,
+            self::PARENT_CATEGORY_ID,
+            self::DESCRIPTION,
+            self::DEFAULT_BREADCRUMBS,
+            self::ADVISORY_TEXT,
+            self::INSPECTION_MANUAL_REFERENCE,
+            $isAdvisory,
+            $isPrs,
+            $isFailure,
+            $deficiencyCategory,
+            $deficiencyCategory === RfrDeficiencyCategoryCode::PRE_EU_DIRECTIVE
         );
 
-        $defect->setInspectionManualReferenceUrl('http://noot.com');
+        $defect->setInspectionManualReferenceUrl(self::INSPECTION_MANUAL_REFERENCE_URL);
+        $defect->setDefectBreadcrumb(self::NEW_BREADCRUMBS);
 
-        $defect->setDefectBreadcrumb('breadcrumb');
+        $this->assertEquals(self::NEW_BREADCRUMBS, $defect->getDefectBreadcrumb());
+        $this->assertEquals(self::DEFECT_ID, $defect->getDefectId());
+        $this->assertEquals(self::PARENT_CATEGORY_ID, $defect->getParentCategoryId());
+        $this->assertEquals(self::DESCRIPTION, $defect->getDescription());
+        $this->assertEquals(self::ADVISORY_TEXT, $defect->getAdvisoryText());
+        $this->assertEquals(self::INSPECTION_MANUAL_REFERENCE, $defect->getInspectionManualReference());
+        $this->assertEquals(self::INSPECTION_MANUAL_REFERENCE_URL, $defect->getInspectionManualReferenceUrl());
 
-        $this->assertEquals('breadcrumb', $defect->getDefectBreadcrumb());
-        $this->assertEquals(10, $defect->getDefectId());
-        $this->assertEquals(20, $defect->getParentCategoryId());
-        $this->assertEquals('Description', $defect->getDescription());
-        $this->assertEquals('Advisory text', $defect->getAdvisoryText());
-        $this->assertEquals('Inspection manual reference', $defect->getInspectionManualReference());
-        $this->assertEquals(true, $defect->isAdvisory());
-        $this->assertEquals(false, $defect->isPrs());
-        $this->assertEquals(true, $defect->isFailure());
-        $this->assertEquals('http://noot.com', $defect->getInspectionManualReferenceUrl());
-        $this->assertEquals(RfrDeficiencyCategoryCode::PRE_EU_DIRECTIVE, $defect->getDeficiencyCategoryCode());
-        $this->assertTrue($defect->isPreEuDirective());
+        $this->assertEquals($isAdvisory, $defect->isAdvisory());
+        $this->assertEquals($isPrs, $defect->isPrs());
+        $this->assertEquals($isFailure, $defect->isFailure());
+
+        $this->assertEquals($deficiencyCategory, $defect->getDeficiencyCategoryCode());
+        $this->assertEquals($deficiencyCategory === RfrDeficiencyCategoryCode::PRE_EU_DIRECTIVE, $defect->isPreEuDirective());
+
+        $isMinor = $deficiencyCategory === RfrDeficiencyCategoryCode::MINOR;
+        $this->assertEquals($isMinor, $defect->isMinorDefect());
+
+        $this->assertEquals($isPrs || $isFailure, $defect->canDisplayFailureButton());
+        $this->assertEquals($isPrs && !$isMinor, $defect->canDisplayPrsButton());
+        $this->assertEquals($isAdvisory, $defect->canDisplayAdvisoryButton());
+    }
+
+    public function testCreationDP()
+    {
+        return [
+            ["isAdvisory" => true, "isPrs" => true, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::PRE_EU_DIRECTIVE],
+            ["isAdvisory" => true, "isPrs" => true, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::DANGEROUS],
+            ["isAdvisory" => true, "isPrs" => true, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::MAJOR],
+            ["isAdvisory" => true, "isPrs" => true, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::MINOR],
+
+            ["isAdvisory" => false, "isPrs" => true, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::PRE_EU_DIRECTIVE],
+            ["isAdvisory" => false, "isPrs" => true, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::DANGEROUS],
+            ["isAdvisory" => false, "isPrs" => true, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::MAJOR],
+            ["isAdvisory" => false, "isPrs" => true, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::MINOR],
+
+            ["isAdvisory" => false, "isPrs" => false, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::PRE_EU_DIRECTIVE],
+            ["isAdvisory" => false, "isPrs" => false, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::DANGEROUS],
+            ["isAdvisory" => false, "isPrs" => false, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::MAJOR],
+            ["isAdvisory" => false, "isPrs" => false, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::MINOR],
+
+            ["isAdvisory" => false, "isPrs" => false, "isFailure" => false, "deficiencyCategory" => RfrDeficiencyCategoryCode::PRE_EU_DIRECTIVE],
+            ["isAdvisory" => false, "isPrs" => false, "isFailure" => false, "deficiencyCategory" => RfrDeficiencyCategoryCode::DANGEROUS],
+            ["isAdvisory" => false, "isPrs" => false, "isFailure" => false, "deficiencyCategory" => RfrDeficiencyCategoryCode::MAJOR],
+            ["isAdvisory" => false, "isPrs" => false, "isFailure" => false, "deficiencyCategory" => RfrDeficiencyCategoryCode::MINOR],
+
+            ["isAdvisory" => true, "isPrs" => false, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::PRE_EU_DIRECTIVE],
+            ["isAdvisory" => true, "isPrs" => false, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::DANGEROUS],
+            ["isAdvisory" => true, "isPrs" => false, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::MAJOR],
+            ["isAdvisory" => true, "isPrs" => false, "isFailure" => true, "deficiencyCategory" => RfrDeficiencyCategoryCode::MINOR],
+
+            ["isAdvisory" => true, "isPrs" => false, "isFailure" => false, "deficiencyCategory" => RfrDeficiencyCategoryCode::PRE_EU_DIRECTIVE],
+            ["isAdvisory" => true, "isPrs" => false, "isFailure" => false, "deficiencyCategory" => RfrDeficiencyCategoryCode::DANGEROUS],
+            ["isAdvisory" => true, "isPrs" => false, "isFailure" => false, "deficiencyCategory" => RfrDeficiencyCategoryCode::MAJOR],
+            ["isAdvisory" => true, "isPrs" => false, "isFailure" => false, "deficiencyCategory" => RfrDeficiencyCategoryCode::MINOR],
+
+            ["isAdvisory" => true, "isPrs" => true, "isFailure" => false, "deficiencyCategory" => RfrDeficiencyCategoryCode::PRE_EU_DIRECTIVE],
+            ["isAdvisory" => true, "isPrs" => true, "isFailure" => false, "deficiencyCategory" => RfrDeficiencyCategoryCode::DANGEROUS],
+            ["isAdvisory" => true, "isPrs" => true, "isFailure" => false, "deficiencyCategory" => RfrDeficiencyCategoryCode::MAJOR],
+            ["isAdvisory" => true, "isPrs" => true, "isFailure" => false, "deficiencyCategory" => RfrDeficiencyCategoryCode::MINOR]
+        ];
     }
 }
