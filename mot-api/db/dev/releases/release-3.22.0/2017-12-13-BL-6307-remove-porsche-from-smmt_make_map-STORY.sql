55c6714322536613239ef5f3391d24e0e9431859
DVSA <dvsa@dvsa.gov.uk>
2017-12-13 10:44:25 +0000

    BL-6307 sql to remove porsche from smmt_make_map table - replicated to MOTH

--- mot-api/db/dev/releases/release-3.22.0/2017-12-13-BL-6307-remove-porsche-from-smmt_make_map-STORY.sql
+++ mot-api/db/dev/releases/release-3.22.0/2017-12-13-BL-6307-remove-porsche-from-smmt_make_map-STORY.sql
@@ -1,7 +1,7 @@
 SET @app_user_id = (SELECT `id` FROM `person` WHERE `user_reference` = 'Static Data' OR `username` = 'static data');
 
 
-DELETE
+DELETE FROM
    `smmt_make_map`
 WHERE
    make = 'PORSCHE';
