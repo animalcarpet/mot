964dbfb68b7a3cf0cb0b65c499592877274bec89
DVSA <dvsa@dvsa.gov.uk>
2017-12-21 09:30:21 +0000

    Update application version do 3.22

---
+++ mot-api/db/dev/populate/test-data/mot_test_current.sql
@@ -0,0 +1,3 @@
+INSERT INTO `mot_test_current` VALUES
+(1,1,2033,10000,NULL,2,1,1,1,date_sub(current_timestamp(), interval 1 hour),current_timestamp(),current_timestamp(),6,current_timestamp(),date_add(current_date(), interval 365 day),NULL,NULL,396949931299,NULL,0,NULL,6093,'km','OK',1,'2015-06-17 14:09:15.604000',1,'2015-10-22 18:55:50.077598',2,'0.0.0.0');
+
